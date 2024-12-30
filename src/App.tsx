74bfba2d51379b8b160585f0fe71f9110d1128c8
setiawanDedi05 <dssetiawan05@gmail.com>
2024-12-30 20:29:11 +0700

    add tailwind and create todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo {
+  title: string;
+  complete: boolean;
 }
 
-export default App
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState("");
+  return (
+    <div className="h-screen w-screen bg-slate-900 flex justify-start items-center py-10 flex-col gap-5">
+      <h1 className="text-slate-300 text-4xl font-bold font-fixel">Productify</h1>
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="type todo"
+          className="py-1 px-2 rounded-s-lg"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button
+          className="bg-slate-300 py-1 px-2 rounded-e-lg"
+          onClick={() => {
+            setTodos((prev: Todo[]) => {
+              const newTodos = [...prev];
+              newTodos.push({ title: todo, complete: false });
+              return newTodos;
+            });
+            setTodo("");
+          }}
+        >
+          save
+        </button>
+      </div>
+      <div className="flex items-center flex-col gap-5">
+        <h2 className="text-2xl text-slate-300">Todo List</h2>
+        <div className="border rounded-md w-full min-w-80 h-full p-5">
+          {todos.map((todo: Todo, index: number) => {
+            return (
+              <div className="flex justify-between" key={index}>
+                {!todo.complete ? (
+                  <span className="text-xl text-slate-300">
+                    {todo.title.toUpperCase()}
+                  </span>
+                ) : (
+                  <del className="text-xl text-slate-300">
+                    {todo.title.toUpperCase()}
+                  </del>
+                )}
+                <input
+                  type="checkbox"
+                  defaultChecked={todo.complete}
+                  onChange={(e) =>
+                    setTodos((prev) => {
+                      const newTodos = [...prev];
+                      newTodos[index].complete = e.target.checked;
+                      return newTodos;
+                    })
+                  }
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
